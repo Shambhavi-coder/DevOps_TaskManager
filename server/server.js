b8061e439fe6691021d0d381660b4777eaec677a
Shambbhavi-coder <shambhavi3jul@gmail.com>
2026-01-10 11:03:19 +0530

    removed unused variable

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ app.get('/health', (req, res) => {
 });
 
 // Error handling middleware
-app.use((err, req, res, next) => {
+// `_next` is intentionally unused to satisfy ESLint
+app.use((err, req, res) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
